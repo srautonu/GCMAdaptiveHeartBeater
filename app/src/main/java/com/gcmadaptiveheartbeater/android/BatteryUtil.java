e96ec05190bf72e3bdcd360eef662388517a177c
Saifur Rahman <saifur80@gmail.com>
2017-03-04 19:40:25 +0600

    BatteryService experimentation (commented out)

---
+++ app/src/main/java/com/gcmadaptiveheartbeater/android/BatteryUtil.java
@@ -0,0 +1,30 @@
+package com.gcmadaptiveheartbeater.android;
+
+/**
+ * Created by mrahman on 04-Mar-17.
+ */
+//import android.os.BatteryManager;
+//import android.content.Context;
+//
+//import static android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY;
+//import static android.os.BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER;
+//import static android.os.BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER;
+//
+//public class BatteryUtil {
+//    public static int getRemainingMicroAmpHour(Context context)
+//    {
+//        BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
+//        return mBatteryManager.getIntProperty(BATTERY_PROPERTY_CHARGE_COUNTER);
+//    }
+//    public static long getRemainingNanoWattHour(Context context)
+//    {
+//        BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
+//        return mBatteryManager.getLongProperty(BATTERY_PROPERTY_ENERGY_COUNTER);
+//    }
+//
+//    public static int getRemainingCapacityPercentage(Context context)
+//    {
+//        BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
+//        return mBatteryManager.getIntProperty(BATTERY_PROPERTY_CAPACITY);
+//    }
+//}
